c7990ceb80fdd7e67345d8b1b50851fd4af65a7b
Alex Davyskiba <adavyskiba@adyax.com>
2018-07-27 17:26:22 +0300

    Fix dashboard.

--- src/App/Handler/HomePageHandler.php
+++ src/App/Handler/HomePageHandler.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Handler;
 
-use App\Entity\Job;
 use App\Entity\Log;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -15,17 +14,31 @@ class HomePageHandler extends BasePageHandler implements RequestHandlerInterface
 {
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        $jobs = $this->storage->getRepository(Job::class)->findAll();
-        $logs = $this->storage->getRepository(Log::class);
-        $data = array_map(function (Job $job) use ($logs) {
-            return [
-                'id' => $job->getId(),
-                'name' => $job->getName(),
-                'logs' => array_map(function (Log $log) {
-                    return $log->jsonSerialize();
-                }, $logs->findBy(['job' => $job->getId()], ['created' => 'DESC'], 100)),
-            ];
-        }, $jobs);
-        return new HtmlResponse($this->template->render('app::home-page', ['data' => $data]));
+        $precise = $request->getQueryParams()['precise'] ?? 'hours';
+        $options = [
+            'hours' => '4 hours ago',
+            'day' => '1 day ago',
+            'week' => '1 week ago',
+            'month' => '1 month ago',
+        ];
+        $time = (new \DateTime($options[$precise]));
+        $dql = 'SELECT log.id FROM App\Entity\Log as log WHERE log.created > :date';
+        $results = $this->storage->createQuery($dql)
+            ->setParameter('date', $time)
+            ->execute();
+        $logIds = array_map(function ($i) {
+            return $i['id'];
+        }, $results);
+        /** @var Log[] $logs */
+        $logs = $this->storage->getRepository(Log::class)->findBy(['id' => $logIds], ['created' => 'DESC']);
+        $data = [];
+        foreach ($logs as $log) {
+            $job = $log->getJob()->getId();
+            if (empty($data[$job])) {
+                $data[$job] = $log->getJob()->jsonSerialize();
+            }
+            $data[$job]['logs'][] = $log->jsonSerialize();
+        }
+        return new HtmlResponse($this->template->render('app::home-page', ['data' => $data, 'precise' => $precise]));
     }
 }
